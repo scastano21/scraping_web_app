c139655f93bd91953e6993a6d871d54fbf2d99a7
scastano21 <49565873+scastano21@users.noreply.github.com>
2025-06-07 18:49:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
+import os
 
+# Ejecutar scraping si no existe el CSV
+if not os.path.exists("data/producto.csv"):
+    import subprocess
+    subprocess.run(["python", "scraping.py"])
+
+# Mostrar datos
 st.title("Precio del Samsung Galaxy S23 Ultra")
-
 df = pd.read_csv("data/producto.csv")
 st.write(df)
